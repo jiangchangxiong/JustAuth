568b72bceaa80f3dfe341274cf0e5b68da843547
姜昌雄 <86907785+jiangchangxiong@users.noreply.github.com>
2024-08-24 16:14:37 +0800

    [Fix]Coding Oauth2集成的用户Scope变更为user:profile了
    详见https://coding.net/help/docs/ecology/develop/scopes.html#scopes

--- src/main/java/me/zhyd/oauth/enums/scope/AuthCodingScope.java
+++ src/main/java/me/zhyd/oauth/enums/scope/AuthCodingScope.java
@@ -17,7 +17,7 @@ public enum AuthCodingScope implements AuthScope {
     /**
      * {@code scope} 含义，以{@code description} 为准
      */
-    USER("user", "读取用户的基本信息", false),
+    USER("user:profile", "读取用户的基本信息", false),
     USER_EMAIL("user:email", "读取用户的邮件", false),
     USER_PHONE("user:phone", "读取用户的手机号", false),
     PROJECT("project", "授权项目信息、项目列表，仓库信息，公钥列表、成员", false),
